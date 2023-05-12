597611eb8f94f6f365b0f8036675dd34e1747468
JaylenAnand <126726097+JaylenAnand@users.noreply.github.com>
2023-05-12 12:28:26 +1200

    Cafe Menu 1.py

--- Cafe Menu 1.py
+++ Cafe Menu 1.py
@@ -1,26 +1,42 @@
 #Version 1: Basic Cafe Menu
 #13/04/23
 
+#Credentials Data
+credentials_data = {
+    "user1": "password1",
+    "user2": "password2",
+    "user3": "password3"
+}
+
 #Welcome Message
 print("Welcome to the Cafe Menu App!")
 
 #User Login or Signup
-existing_user = input("Do you have an account with us? (y/n): ")
-if existing_user.lower() == 'y':
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
-    #TODO: Check username and password in index data
-else:
-    while True:
+while True:
+    existing_user = input("Do you have an account with us? (y/n): ")
+    if existing_user.lower() == 'y':
+        username = input("Please enter your username: ")
+        password = input("Please enter your password: ")
+        if credentials_data.get(username) == password:
+            print("Login successful.")
+            break
+        else:
+            print("Invalid Username or Password.")
+    else:
         age = int(input("Please enter your age: "))
         if age < 13 or age > 18:
             print("Sorry, this app is only for students between the ages of 13 and 18.")
         else:
-            username = input("Please enter a username: ")
-            password = input("Please enter a password: ")
-            #TODO: Save username and password in a list
-            break
-    
+            while True:
+                username = input("Please enter a username: ")
+                if username in credentials_data:
+                    print("Username already exists. Please choose a different username.")
+                else:
+                    password = input("Please enter a password: ")
+                    credentials_data[username] = password
+                    print("Account created successfully.")
+                    break
+
 #Display Cafe Menu
 print("Here's our menu:")
 print("1. Coffee - $2.50")
@@ -30,7 +46,7 @@ print("4. Muffin - $2.00")
 print("5. Salad - $4.50")
 
 #Place Order
-order = []  #List to store the order items and quantities
+order = [] #List to store the order items and quantities
 while True:
     item = input("Enter the item number you would like to order (press q to quit): ")
     if item.lower() == 'q':
@@ -39,8 +55,11 @@ while True:
         print("Invalid item number. Please enter a number between 1 and 5.")
         continue
     quantity = int(input("Enter the quantity: "))
+    if quantity <= 0:
+        print("Invalid quantity. Please enter a positive integer.")
+        continue
     order.append((item, quantity))
-    
+
 #Generate Invoice
 print("Invoice:")
 total = 0
@@ -66,3 +85,4 @@ for item, quantity in order:
     print(f"{item_name} x {quantity} - ${quantity*item_price:.2f}")
     total += quantity*item_price
 print(f"Total: ${total:.2f}")
+
